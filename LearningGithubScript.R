a6ce2cd21866ff3eb0b7b7a8fda44429fbf3a684
Kiera ODonnell <klo30@duke.edu>
2024-05-14 10:26:52 -0400

    Created a new r-script and project

---
+++ LearningGithubScript.R
@@ -0,0 +1,23 @@
+# github training with Heili!!
+# 5/14/24
+
+# Github Duke Thinkpad token:
+# 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
+# package we are working with is gitcreds
+gitcreds::gitcreds_set() # this will prompt you to enter your token in the console
+
+#Resources I am using in this Learning Github lesson:
+# Github help: https://happygitwithr.com/
+# Heili's guide: https://github.com/hlowman/TidyTuesday/blob/master/resources/Git_GitHub_Guide.pdf
+
+# I have Git downloaded on the desktop locally, and github is the web browser cloud
+# Create a new repository in github, 
+# Once we have everything downloaded, we create a new R project and select the option "Version Control" - this adds the "Git" tab, make sure this is there before continuing with Github 
+
+
+
+# Lets begin:
+# My favorite food is burgers
+
+
+
